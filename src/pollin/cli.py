dd8e2e177e67538111924d7ddad9b3755da6385f
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-06-24 10:07:44 +0200

    changed default logging back to info
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/215

--- src/pollin/cli.py
+++ src/pollin/cli.py
@@ -28,7 +28,7 @@ def cli(host: str, directory: str, project: str):
     :param directory: Path of the view template directory
     :param project: Abbreviation of the project
     """
-    logging.basicConfig( encoding='utf-8', level=logging.DEBUG)
+    logging.basicConfig( encoding='utf-8', level=logging.INFO)
 
     # setting up the application context
     (AppInitializer(app_context)
