7261f53a38ff4cfe585c38baf5514dce5bdafd00
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-08-21 11:03:42 +0200

    moved loading of external config to configure method
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/266

--- src/pollin/System/init/AppInitializer.py
+++ src/pollin/System/init/AppInitializer.py
@@ -37,6 +37,13 @@ class AppInitializer:
         app_config.ENV = AppEnv(GAMS_API_ORIGIN=app_config.gams_host, PROJECT_ABBR=app_config.project)
         self.app_context.set_config(app_config)
 
+        # load possible external configuration
+        external_config = ApplicationExternalConfigImporter(self.app_context).import_config()
+        if external_config:
+            external_config_parsed = ApplicationExternalConfig(external_config)
+            self.app_context.get_config().project_external_config = external_config_parsed
+            logging.info(f"External configuration loaded {external_config}")
+
         return self
 
     def init_context_beans(self):
@@ -58,13 +65,6 @@ class AppInitializer:
             self.app_context.get_pyrilo().configure(
                 self.app_context.get_config().gams_host,
             "api/v1")
-
-        # load possible external configuration
-        external_config = ApplicationExternalConfigImporter(self.app_context).import_config()
-        if external_config:
-            external_config_parsed = ApplicationExternalConfig(external_config)
-            self.app_context.get_config().project_external_config = external_config_parsed
-            logging.info(f"External configuration loaded {external_config}")
 
         return self
 
