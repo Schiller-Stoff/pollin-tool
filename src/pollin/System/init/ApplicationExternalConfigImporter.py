5575188f4d8194ba4b1bb7729eb4023d60c41283
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-08-21 12:24:52 +0200

    commented out code that is not production ready
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/266

--- src/pollin/System/init/ApplicationExternalConfigImporter.py
+++ src/pollin/System/init/ApplicationExternalConfigImporter.py
@@ -42,28 +42,30 @@ class ApplicationExternalConfigImporter:
             cur_mode = self.app_context.get_config().mode
 
             # toml validation
-            if ApplicationExternalConfig.PROJECT_PROPERTY not in config_toml_dict:
-                raise ValueError(f"'{ApplicationExternalConfig.PROJECT_PROPERTY}' property not found (or empty) in the configuration file: {config_toml_path}")
-            else:
-                if not config_toml_dict.get(ApplicationExternalConfig.PROJECT_PROPERTY).get(ApplicationExternalConfig.PROJECT_ABBR_PROPERTY):
-                    raise ValueError(f"'{ApplicationExternalConfig.PROJECT_PROPERTY}.{ApplicationExternalConfig.PROJECT_ABBR_PROPERTY}' not found (or empty) in configuration file: {config_toml_path}")
+
+            # if ApplicationExternalConfig.PROJECT_PROPERTY not in config_toml_dict:
+            #     raise ValueError(f"'{ApplicationExternalConfig.PROJECT_PROPERTY}' property not found (or empty) in the configuration file: {config_toml_path}")
+            # else:
+            #     if not config_toml_dict.get(ApplicationExternalConfig.PROJECT_PROPERTY).get(ApplicationExternalConfig.PROJECT_ABBR_PROPERTY):
+            #         raise ValueError(f"'{ApplicationExternalConfig.PROJECT_PROPERTY}.{ApplicationExternalConfig.PROJECT_ABBR_PROPERTY}' not found (or empty) in configuration file: {config_toml_path}")
 
             if not config_toml_dict.get(cur_mode):
                 raise ValueError(f"For the currently active mode: '{cur_mode}' is no (or empty) toml property '{cur_mode}' defined. In config toml file: {config_toml_path} ")
 
-            if not config_toml_dict.get(cur_mode).get(ApplicationExternalConfig.MODE_BASE_PROPERTY):
-                raise ValueError(f"Cannot find (or empty) required property {cur_mode}.{ApplicationExternalConfig.MODE_BASE_PROPERTY} in config toml file: {config_toml_path}")
+            # if not config_toml_dict.get(cur_mode).get(ApplicationExternalConfig.MODE_BASE_PROPERTY):
+            #     raise ValueError(f"Cannot find (or empty) required property {cur_mode}.{ApplicationExternalConfig.MODE_BASE_PROPERTY} in config toml file: {config_toml_path}")
+            #
+            # if not config_toml_dict.get(cur_mode).get(ApplicationExternalConfig.MODE_BASE_PROPERTY).get(ApplicationExternalConfig.MODE_BASE_GAMS_API_HOST_PROPERTY):
+            #     raise ValueError(f"Cannot find (or empty) required property {cur_mode}.{ApplicationExternalConfig.MODE_BASE_GAMS_API_HOST_PROPERTY} in config toml file: {config_toml_path}")
+            #
+            # if not config_toml_dict.get(cur_mode).get(ApplicationExternalConfig.MODE_BASE_PROPERTY).get(ApplicationExternalConfig.MODE_BASE_OUTPUT_PATH_PROPERTY):
+            #     raise ValueError(f"Cannot find (or empty) required property {cur_mode}.{ApplicationExternalConfig.MODE_BASE_OUTPUT_PATH_PROPERTY} in config toml file: {config_toml_path}")
+            #
+            # if not config_toml_dict.get(cur_mode).get(ApplicationExternalConfig.MODE_BASE_PROPERTY).get(ApplicationExternalConfig.MODE_BASE_SRC_PATH_PROPERTY):
+            #     raise ValueError(f"Cannot find (or empty) required property {cur_mode}.{ApplicationExternalConfig.MODE_BASE_SRC_PATH_PROPERTY} in config toml file: {config_toml_path}")
 
-            if not config_toml_dict.get(cur_mode).get(ApplicationExternalConfig.MODE_BASE_PROPERTY).get(ApplicationExternalConfig.MODE_BASE_GAMS_API_HOST_PROPERTY):
-                raise ValueError(f"Cannot find (or empty) required property {cur_mode}.{ApplicationExternalConfig.MODE_BASE_GAMS_API_HOST_PROPERTY} in config toml file: {config_toml_path}")
 
-            if not config_toml_dict.get(cur_mode).get(ApplicationExternalConfig.MODE_BASE_PROPERTY).get(ApplicationExternalConfig.MODE_BASE_OUTPUT_PATH_PROPERTY):
-                raise ValueError(f"Cannot find (or empty) required property {cur_mode}.{ApplicationExternalConfig.MODE_BASE_OUTPUT_PATH_PROPERTY} in config toml file: {config_toml_path}")
-
-            if not config_toml_dict.get(cur_mode).get(ApplicationExternalConfig.MODE_BASE_PROPERTY).get(ApplicationExternalConfig.MODE_BASE_SRC_PATH_PROPERTY):
-                raise ValueError(f"Cannot find (or empty) required property {cur_mode}.{ApplicationExternalConfig.MODE_BASE_SRC_PATH_PROPERTY} in config toml file: {config_toml_path}")
-
-
+            # TODO return complete config file not only mode dictionary
 
             # default to empty dict if mode not found
             return config_toml_dict.get(cur_mode, {})
