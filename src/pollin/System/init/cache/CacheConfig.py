0589539abee1fbce06996940ec364c369e76084e
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-08-22 14:30:06 +0200

    added cashing classes
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/201

---
+++ src/pollin/System/init/cache/CacheConfig.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class CacheConfig:
+    """Configuration for local data caching"""
+    cache_dir: Path
+    enabled: bool = True
+
+    @property
+    def objects_cache_file(self) -> Path:
+        return self.cache_dir / "objects_cache.json"
+
+    @property
+    def project_cache_file(self) -> Path:
+        return self.cache_dir / "project_cache.json"
+
+    @property
+    def cache_metadata_file(self) -> Path:
+        return self.cache_dir / "cache_metadata.json"
