fe5856352bc4ab88b4529c5803229220f3964820
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-09-16 11:24:23 +0200

    moved logic of calling the build command to test fixture and removed logic from test bodies
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/269

--- tests/integration_tests/test_build_cmd.py
+++ tests/integration_tests/test_build_cmd.py
@@ -9,38 +9,32 @@ from utils.TestProject import TestProject
 def test_existence_of_expected_html_output_files(mock_pollin_env):
     """Integration test for the build command."""
 
-    runner = CliRunner()
-    with runner.isolated_filesystem(mock_pollin_env.project_dir):
-        # Run build command
-        result = runner.invoke(cli, ['build', str(mock_pollin_env.project_dir)])
+    cli_result, pollin_project = mock_pollin_env
 
-        assert result.exit_code == 0, f"Build command failed with exit code {result.exit_code} and output: {result.output}"
+    assert cli_result.exit_code == 0, f"Build command failed with exit code {cli_result.exit_code} and output: {cli_result.output}"
 
-        # Check that the output directory and files are created
-        assert os.path.exists(mock_pollin_env.get_config().project_public_dir), "Public directory not found"
-        assert os.path.exists(mock_pollin_env.get_config().project_public_static_dir), "Project static directory not found"
-        assert os.path.exists(mock_pollin_env.get_config().project_public_dir / 'index.html'), "Index file not found"
-        assert os.path.exists(mock_pollin_env.get_config().project_public_dir / 'objects'), "Objects directory not found"
-        assert os.path.exists(mock_pollin_env.get_config().project_public_dir / 'objects' / 'test.1' / 'index.html'), "Object file not found"
+    # Check that the output directory and files are created
+    assert os.path.exists(pollin_project.get_config().project_public_dir), "Public directory not found"
+    assert os.path.exists(pollin_project.get_config().project_public_static_dir), "Project static directory not found"
+    assert os.path.exists(pollin_project.get_config().project_public_dir / 'index.html'), "Index file not found"
+    assert os.path.exists(pollin_project.get_config().project_public_dir / 'objects'), "Objects directory not found"
+    assert os.path.exists(pollin_project.get_config().project_public_dir / 'objects' / 'test.1' / 'index.html'), "Object file not found"
 
 def test_existence_of_expected_static_files(mock_pollin_env):
     """Integration test for the build command."""
 
-    runner = CliRunner()
-    with runner.isolated_filesystem(mock_pollin_env.project_dir):
-        # Run build command
-        result = runner.invoke(cli, ['build', str(mock_pollin_env.project_dir)])
+    cli_result, pollin_project = mock_pollin_env
 
-        assert result.exit_code == 0, f"Build command failed with exit code {result.exit_code} and output: {result.output}"
+    assert cli_result.exit_code == 0, f"Build command failed with exit code {cli_result.exit_code} and output: {cli_result.output}"
 
-        # Check that static files are copied
-        assert os.path.exists(mock_pollin_env.get_config().project_public_dir), "Public directory not found"
-        assert os.path.exists(mock_pollin_env.get_config().project_public_static_dir), "Project static directory not found"
-        assert os.path.exists(mock_pollin_env.get_test_css_path()), "CSS file not found"
-        assert os.path.exists(mock_pollin_env.get_test_js_path()), "JS file not found"
-        assert os.path.exists(mock_pollin_env.get_test_logo_path()), "Logo file not found"
+    # Check that static files are copied
+    assert os.path.exists(pollin_project.get_config().project_public_dir), "Public directory not found"
+    assert os.path.exists(pollin_project.get_config().project_public_static_dir), "Project static directory not found"
+    assert os.path.exists(pollin_project.get_test_css_path()), "CSS file not found"
+    assert os.path.exists(pollin_project.get_test_js_path()), "JS file not found"
+    assert os.path.exists(pollin_project.get_test_logo_path()), "Logo file not found"
 
-        assert not os.path.exists(mock_pollin_env.get_config().project_public_dir / 'objects' / 'test.123123' / 'index.html'), "Object file should not exist"
+    assert not os.path.exists(pollin_project.get_config().project_public_dir / 'objects' / 'test.123123' / 'index.html'), "Object file should not exist"
 
 
 def test_build_command_fails_when_invalid_command_is_given(mock_pollin_env):
@@ -74,90 +68,68 @@ def test_build_command_fails_when_no_config_file_was_found_at_path(tmp_path):
 
 def test_object_template_contains_expected_values(mock_pollin_env):
 
-    runner = CliRunner()
-    with runner.isolated_filesystem(mock_pollin_env.project_dir):
-        # Run build command
-        result = runner.invoke(cli, ['build', str(mock_pollin_env.project_dir)])
+    cli_result, pollin_project = mock_pollin_env
+    assert cli_result.exit_code == 0, f"Build command failed with exit code {cli_result.exit_code} and output: {cli_result.output}"
 
-        assert result.exit_code == 0, f"Build command failed with exit code {result.exit_code} and output: {result.output}"
-
-        object_html = (mock_pollin_env.get_config().project_public_dir / 'objects' / TestDigitalObject.ID / 'index.html').read_text()
-        assert TestDigitalObject.TITLE in object_html, "Object title not found in object HTML"
+    object_html = (pollin_project.get_config().project_public_dir / 'objects' / TestDigitalObject.ID / 'index.html').read_text()
+    assert TestDigitalObject.TITLE in object_html, "Object title not found in object HTML"
 
 
 def test_project_template_contains_expected_values(mock_pollin_env):
     """Integration test for the build command."""
 
-    runner = CliRunner()
-    with runner.isolated_filesystem(mock_pollin_env.project_dir):
-        # Run build command
-        result = runner.invoke(cli, ['build', str(mock_pollin_env.project_dir)])
+    cli_result, pollin_project = mock_pollin_env
+    assert cli_result.exit_code == 0, f"Build command failed with exit code {cli_result.exit_code} and output: {cli_result.output}"
 
-        assert result.exit_code == 0, f"Build command failed with exit code {result.exit_code} and output: {result.output}"
-
-        # Check that the output files contain expected values
-        index_html = (mock_pollin_env.get_config().project_public_dir / 'index.html').read_text()
-        assert mock_pollin_env.PROJECT_ABBR in index_html, "Project abbreviation not found in index.html"
+    # Check that the output files contain expected values
+    index_html = (pollin_project.get_config().project_public_dir / 'index.html').read_text()
+    assert pollin_project.PROJECT_ABBR in index_html, "Project abbreviation not found in index.html"
 
 
 def test_object_template_includes_expected_object_to_string(mock_pollin_env):
     """Integration test for the build command."""
 
-    runner = CliRunner()
-    with runner.isolated_filesystem(mock_pollin_env.project_dir):
-        # Run build command
-        result = runner.invoke(cli, ['build', str(mock_pollin_env.project_dir)])
+    cli_result, pollin_project = mock_pollin_env
+    assert cli_result.exit_code == 0, f"Build command failed with exit code {cli_result.exit_code} and output: {cli_result.output}"
 
-        assert result.exit_code == 0, f"Build command failed with exit code {result.exit_code} and output: {result.output}"
-
-        # Check that the output files contain expected values
-        object_html = (mock_pollin_env.get_config().project_public_dir / 'objects' / TestDigitalObject.ID / 'index.html').read_text()
-        test_object_to_string = str(TestDigitalObject.generate())
-        assert test_object_to_string in object_html, "Object __str__ value not found in object HTML"
+    # Check that the output files contain expected values
+    object_html = (pollin_project.get_config().project_public_dir / 'objects' / TestDigitalObject.ID / 'index.html').read_text()
+    test_object_to_string = str(TestDigitalObject.generate())
+    assert test_object_to_string in object_html, "Object __str__ value not found in object HTML"
 
 def test_project_template_includes_expected_project_to_string(mock_pollin_env):
     """Integration test for the build command."""
 
-    runner = CliRunner()
-    with runner.isolated_filesystem(mock_pollin_env.project_dir):
-        # Run build command
-        result = runner.invoke(cli, ['build', str(mock_pollin_env.project_dir)])
+    cli_result, pollin_project = mock_pollin_env
+    assert cli_result.exit_code == 0, f"Build command failed with exit code {cli_result.exit_code} and output: {cli_result.output}"
 
-        assert result.exit_code == 0, f"Build command failed with exit code {result.exit_code} and output: {result.output}"
-
-        # Check that the output files contain expected values
-        index_html = (mock_pollin_env.get_config().project_public_dir / 'index.html').read_text()
-        test_project_to_string = str(TestProject.generate())
-        assert test_project_to_string in index_html, "Project __str__ value not found in index.html"
+    # Check that the output files contain expected values
+    index_html = (pollin_project.get_config().project_public_dir / 'index.html').read_text()
+    test_project_to_string = str(TestProject.generate())
+    assert test_project_to_string in index_html, "Project __str__ value not found in index.html"
 
 
 def test_object_list_template_contains_expected_values(mock_pollin_env):
     """Integration test for the build command."""
 
-    runner = CliRunner()
-    with runner.isolated_filesystem(mock_pollin_env.project_dir):
-        # Run build command
-        result = runner.invoke(cli, ['build', str(mock_pollin_env.project_dir)])
+    cli_result, pollin_project = mock_pollin_env
 
-        assert result.exit_code == 0, f"Build command failed with exit code {result.exit_code} and output: {result.output}"
+    assert cli_result.exit_code == 0, f"Build command failed with exit code {cli_result.exit_code} and output: {cli_result.output}"
 
-        # Check that the output files contain expected values
-        object_list_html = (mock_pollin_env.get_config().project_public_dir / 'objects' / 'index.html').read_text()
-        assert TestDigitalObject.TITLE in object_list_html, "Object title not found in object list HTML"
-        assert TestDigitalObject.ID in object_list_html, "Object ID not found in object list HTML"
+    # Check that the output files contain expected values
+    object_list_html = (pollin_project.get_config().project_public_dir / 'objects' / 'index.html').read_text()
+    assert TestDigitalObject.TITLE in object_list_html, "Object title not found in object list HTML"
+    assert TestDigitalObject.ID in object_list_html, "Object ID not found in object list HTML"
 
 
 def test_object_list_templates_includes_expected_to_string(mock_pollin_env):
     """Integration test for the build command."""
 
-    runner = CliRunner()
-    with runner.isolated_filesystem(mock_pollin_env.project_dir):
-        # Run build command
-        result = runner.invoke(cli, ['build', str(mock_pollin_env.project_dir)])
+    cli_result, pollin_project = mock_pollin_env
 
-        assert result.exit_code == 0, f"Build command failed with exit code {result.exit_code} and output: {result.output}"
+    assert cli_result.exit_code == 0, f"Build command failed with exit code {cli_result.exit_code} and output: {cli_result.output}"
 
-        # Check that the output files contain expected values
-        object_list_html = (mock_pollin_env.get_config().project_public_dir / 'objects' / 'index.html').read_text()
-        test_object_to_string = str(TestDigitalObject.generate())
-        assert test_object_to_string in object_list_html, "Object __str__ value not found in object list HTML"
+    # Check that the output files contain expected values
+    object_list_html = (pollin_project.get_config().project_public_dir / 'objects' / 'index.html').read_text()
+    test_object_to_string = str(TestDigitalObject.generate())
+    assert test_object_to_string in object_list_html, "Object __str__ value not found in object list HTML"
