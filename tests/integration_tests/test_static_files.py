b7d7021812092d93d760b1effeca9aeacc7182ed
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-09-16 11:27:45 +0200

    added test for included logo file
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/269

--- tests/integration_tests/test_static_files.py
+++ tests/integration_tests/test_static_files.py
@@ -16,3 +16,12 @@ def test_produces_expected_css_file(mock_pollin_env):
     assert css_file.exists(), "Expected CSS file not found"
     css_content = css_file.read_text()
     assert pollin_project.TEST_CSS_FILE_CONTENT in css_content, "CSS file does not contain expected content"
+
+def test_produces_expected_image_file(mock_pollin_env):
+    cli_result, pollin_project = mock_pollin_env
+    assert cli_result.exit_code == 0, f"Build command failed with exit code {cli_result.exit_code} and output: {cli_result.output}"
+    # Check that the output files contain expected values
+    image_file = pollin_project.get_test_logo_path()
+    assert image_file.exists(), "Expected image file not found"
+    image_content = image_file.read_bytes()
+    assert image_content == pollin_project.TEST_LOGO_FILE_CONTENT, "Image file does not contain expected content"
