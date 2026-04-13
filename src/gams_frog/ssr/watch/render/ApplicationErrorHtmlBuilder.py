55ca94d731b81a56dd535fe2081df96cc6d4104e
Sebi <SebastianStoff@gmx.at>
2026-04-13 11:55:33 +0200

    renamed comments etc.
    https://zimlab.uni-graz.at/gams5/production/postprocessing/gams-frog/-/work_items/20

--- src/gams_frog/ssr/watch/render/ApplicationErrorHtmlBuilder.py
+++ src/gams_frog/ssr/watch/render/ApplicationErrorHtmlBuilder.py
@@ -7,7 +7,7 @@ class ApplicationErrorHtmlBuilder:
 
     """
 
-    _GENERAL_ERROR_TEMPLATE = "<html><body style='padding: 1em 5em; font-family: MONOSPACE; font-size:1.2em; line-height:1.5em;'><div style='max-width: 75%'><h1 style='color: red'>POLLIN ERROR</h1> <p>Occurred at static site generation (template-rendering)</p><p>{}</p></div></body></html>"
+    _GENERAL_ERROR_TEMPLATE = "<html><body style='padding: 1em 5em; font-family: MONOSPACE; font-size:1.2em; line-height:1.5em;'><div style='max-width: 75%'><h1 style='color: red'>GAMS_FROG ERROR</h1> <p>Occurred at static site generation (template-rendering)</p><p>{}</p></div></body></html>"
 
     @staticmethod
     def build_general_error_html(msg):
